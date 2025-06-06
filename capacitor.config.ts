83b588f5aedc50344caaee7857712297571607ca
TassiaBarbosa <tassiabarbosa@souunisuam.com.br>
2025-06-06 17:01:46 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ionic.petshop_app',
   appName: 'petshop-app',
   webDir: 'www'
 };
