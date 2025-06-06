1fd71c48d9ec1f22fc26a79b1e3c5501b1a8466e
TassiaBarbosa <tassiabarbosa@souunisuam.com.br>
2025-06-06 16:24:46 -0300

    Update cadastro.module.ts

--- src/app/cadastro/cadastro.module.ts
+++ src/app/cadastro/cadastro.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { CadastroPageRoutingModule } from './cadastro-routing.module';
-
 import { CadastroPage } from './cadastro.page';
+//
+import { CadastroFormComponent } from '../cadastro-form/cadastro-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     CadastroPageRoutingModule
   ],
-  declarations: [CadastroPage]
+  declarations: [CadastroPage, CadastroFormComponent]
 })
 export class CadastroPageModule {}
